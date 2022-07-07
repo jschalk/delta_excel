a242174696a45b34c725e95a9580745ce3fba7d2
Jo Schalk <joachim.schalk@gmail.com>
2022-07-07 09:22:39 -0700

    Added comments for clarity

--- main.py
+++ main.py
@@ -3,41 +3,53 @@
 # Step 0 The Excel sheet is opened and converted to a pandas dataframe.
 # Step 1 Each row is evaluated. Users can add logic here
 # Step 2 Convert the dataframe object to an Excel file and save with timestamp in name.
-import os
-import pandas as pd
 
-pd.io.formats.excel.ExcelFormatter.header_style = None
+# "os" library is for manipulating files on your computer
+import os
+
+# "pandas" is for manipulating data, in this case Excel files
+import pandas
+
+# this makes it so the saved excel files don't have weird formatting
+pandas.io.formats.excel.ExcelFormatter.header_style = None
 
 
 def main(name):
     print(f"Hi, {name}")
-    dir_src = os.curdir
     file1_src = "C:/dev/excel_delta/venv/ExampleData/StudentFood.xlsx"
     file1_dst = "C:/dev/excel_delta/venv/ExampleData/StudentFood_changed.xlsx"
     file1_sheet1 = "Sheet1"
-    df = pd.read_excel(io=file1_src, sheet_name=file1_sheet1)
-    # print(f"Type: {type(df)}")
+
+    # creates a pandas "dataframe" object that's basically the Excel file sheet
+    df = pandas.read_excel(io=file1_src, sheet_name=file1_sheet1)
+
+    # prints to console the Excel file path
     print(f"Excel from {file1_dst}")
     print(df)
 
     df.reset_index()
 
-    for index in df.index:
-        if df.at[index, "Student Name"] == "Li Ling":
-            df.at[index, "Student Name"] = "Li Ping"
+    # goes through the Excel file one row at time
+    for row in df.index:
+        # corrects student name
+        if df.at[row, "Student Name"] == "Li Ling":
+            df.at[row, "Student Name"] = "Li Ping"
 
-        if df.at[index, "Food"] == "Frys":
-            df.at[index, "Food"] = "Fries"
-        elif df.at[index, "Food"] == "Cola":
-            temp_x = df.at[index, "Food"]
-            df.at[index, "Food"] = df.at[index, "Drink"]
-            df.at[index, "Drink"] = temp_x
+        # corrects food column misspellings
+        if df.at[row, "Food"] == "Frys":
+            df.at[row, "Food"] = "Fries"
+        elif df.at[row, "Food"] == "Cola":
+            temp_x = df.at[row, "Food"]
+            df.at[row, "Food"] = df.at[row, "Drink"]
+            df.at[row, "Drink"] = temp_x
 
-        if df.at[index, "Drink"] == "H20":
-            df.at[index, "Drink"] = "Water"
+        # corrects drink errors
+        if df.at[row, "Drink"] == "H20":
+            df.at[row, "Drink"] = "Water"
 
-        if df.at[index, "Age"] > 20:
-            df.at[index, "Age"] = df.at[index, "Age"] / 10
+        # corrects age error
+        if df.at[row, "Age"] > 20:
+            df.at[row, "Age"] = df.at[row, "Age"] / 10
 
     print(f"\n")
     print(f"Saved to {file1_dst}")
@@ -46,8 +58,6 @@ def main(name):
     df.to_excel(file1_dst, index=False, sheet_name="sch_data_1")
 
 
-# Press the green button in the gutter to run the script.
+# if the script is run this runs
 if __name__ == "__main__":
     main("Portland Public Schools")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
